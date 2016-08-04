937c90df21f5ad0e0fd75f9b8cfa0f1f585dc359
Antonio C. de Carvalho Junior <antonio.junior@tcu.gov.br>
2016-08-04 19:39:26 -0300

    Insere linha de comentário.

--- src/main/java/br/gov/tcu/bbt/outro/ClassePublicaMasNaoUsada.java
+++ src/main/java/br/gov/tcu/bbt/outro/ClassePublicaMasNaoUsada.java
@@ -3,6 +3,7 @@ package br.gov.tcu.bbt.outro;
 /**
  * Se a classe não é usada por nenhum módulo aberto, o IntelliJ acusa. Caso seja um falso positivo,
  * \@SuppressWarnings pode ser usado.
+ *
  */
 public class ClassePublicaMasNaoUsada {
 }
