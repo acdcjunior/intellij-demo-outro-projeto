2e3426fffe8f217fdc4f81e800a1643e488e9e9b
Antonio C. de Carvalho Junior <antonio.junior@tcu.gov.br>
2016-08-04 22:50:17 -0300

    Inclui smart types

---
+++ src/main/java/br/gov/tcu/bbt/outro/dica103/Agua.java
@@ -0,0 +1,7 @@
+package br.gov.tcu.bbt.outro.dica103;
+
+interface Agua {}
+
+class Squirtle implements Agua {}
+class Blastoise implements Agua {}
+class PsyDuck implements Agua {}
---
+++ src/main/java/br/gov/tcu/bbt/outro/dica103/SmartTypes.java
@@ -0,0 +1,17 @@
+package br.gov.tcu.bbt.outro.dica103;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+@SuppressWarnings("ALL")
+public class SmartTypes {
+
+    private Set<String> meuSet = null;
+    private List<String> minhaList = null;
+    private Map<String, String> meuMap = null;
+
+    private Agua deAgua = null;
+
+}
+
